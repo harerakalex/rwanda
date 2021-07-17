619c2e7a62ebd49b4f032afe879c2adf24c747ac
harerakalex <hareraloston@gmail.com>
2021-07-17 20:19:17 +0200

    Add all sectors of nyarugenge district

---
+++ src/provinces/kigali/districts/nyarugenge/sectors/nyakabanda.py
@@ -0,0 +1,39 @@
+''' Nyakabanda sector '''
+
+nyakabanda = {
+  "Munanira I": [
+    "Kabusunzu",
+    "Munanira",
+    "Ntaraga",
+    "Nyagasozi",
+    "Rurembo"
+  ],
+  "Munanira II": [
+    "Gasiza",
+    "Kamwiza",
+    "Kanyange",
+    "Karudandi",
+    "Kigabiro",
+    "Kokobe",
+    "Mucyuranyana",
+    "Nkundumurimbo"
+  ],
+  "Nyakabanda I": [
+    "Akinkware",
+    "Gapfupfu",
+    "Gasiza",
+    "Kariyeri",
+    "Kokobe",
+    "Munini",
+    "Nyakabanda",
+    "Rwagitanga"
+  ],
+  "Nyakabanda II": [
+    "Ibuhoro",
+    "Kabeza",
+    "Kanyiranganji",
+    "Karujongi",
+    "Kigarama",
+    "Kirwa"
+  ]
+}
---
+++ src/provinces/kigali/districts/nyarugenge/sectors/nyamirambo.py
@@ -0,0 +1,46 @@
+''' Nyamirambo Sector'''
+
+nyamirambo = {
+  "Cyivugiza": [
+    "Amizero",
+    "Gabiro",
+    "Imanzi",
+    "Ingenzi",
+    "Intwari",
+    "Karisimbi",
+    "Mahoro",
+    "Mpano",
+    "Muhabura",
+    "Muhoza",
+    "Munini",
+    "Rugero",
+    "Shema"
+  ],
+  "Gasharu": [
+    "Kagunga",
+    "Karukoro",
+    "Rwintare"
+  ],
+  "Mumena": [
+    "Akanyana",
+    "Akanyirazaninka",
+    "Akarekare",
+    "Akatabaro",
+    "Irembo",
+    "Itaba",
+    "Kiberinka",
+    "Mumena",
+    "Rwampara"
+  ],
+  "Rugarama": [
+    "Gatare",
+    "Kiberinka",
+    "Munanira",
+    "Riba",
+    "Rubona",
+    "Rugarama",
+    "Runyinya",
+    "Rusisiro",
+    "Tetero"
+  ]
+}
