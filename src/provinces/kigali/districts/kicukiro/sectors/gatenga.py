79f4b543185d8995b12b423a4bc5189c312214fc
harerakalex <hareraloston@gmail.com>
2021-07-17 21:08:43 +0200

    Add all sectors of kicukiro

---
+++ src/provinces/kigali/districts/kicukiro/sectors/gatenga.py
@@ -0,0 +1,45 @@
+''' Gatenga sector '''
+
+gatenga = {
+  "Gatenga": [
+    "Amahoro",
+    "Gakoki",
+    "Gatenga",
+    "Ihuriro",
+    "Isangano",
+    "Rugari"
+  ],
+  "Karambo": [
+    "Gwiza",
+    "Ihuriro",
+    "Jyambere",
+    "Kamabuye",
+    "Mahoro",
+    "Ramiro",
+    "Rebero",
+    "Rugwiro",
+    "Ruhuka",
+    "Sangwa"
+  ],
+  "Nyanza": [
+    "Bwiza",
+    "Cyeza",
+    "Gasabo",
+    "Ihuriro",
+    "Isonga",
+    "Juru",
+    "Marembo",
+    "Murambi",
+    "Nyanza",
+    "Rebero",
+    "Rusororo",
+    "Sabaganga",
+    "Taba"
+  ],
+  "Nyarurama": [
+    "Bigo",
+    "Bisambu",
+    "Kabeza",
+    "Nyabikenke"
+  ]
+}
---
+++ src/provinces/kigali/districts/kicukiro/sectors/kicukiro.py
@@ -0,0 +1,33 @@
+''' Kicukiro sector '''
+
+kicukiro = {
+  "Gasharu": [
+    "Amajyambere",
+    "Gasharu",
+    "Sakirwa",
+    "Umunyinya"
+  ],
+  "Kagina": [
+    "Gashiha",
+    "Iriba",
+    "Multimedia",
+    "Umunyinya",
+    "Umuremure",
+    "Urugero"
+  ],
+  "Kicukiro": [
+    "Gasave",
+    "Isoko",
+    "Karisimbi",
+    "Kicukiro",
+    "Triangle",
+    "Ubumwe"
+  ],
+  "Ngoma": [
+    "Ahitegeye",
+    "Intaho",
+    "Iriba",
+    "Isangano",
+    "Urugero"
+  ]
+}
