cb97bf8de209bc4c7a4474239c65aaf3cf3ef781
harerakalex <hareraloston@gmail.com>
2021-07-17 19:16:38 +0200

    Add get_cells and get_villages functions

--- src/__init__.py
+++ src/__init__.py
@@ -3,3 +3,6 @@ from main import *
 print('Get Provinces====', get_provinces())
 print('Get Districts====', get_districts('kiGali'))
 print('Get sectors====', get_sectors('kigali', 'nyarugenge'))
+print('Get Cells====', get_cells('kigaLI', district='Nyarugenge', sector='nyarugenge'))
+print('Get villages====', get_villages('kigaLI', district='Nyarugenge', sector='nyarugenge', cell='rwampara'))
+
--- src/main.py
+++ src/main.py
@@ -23,3 +23,21 @@ def get_sectors(province = '', district = ''):
             return [sector for province in data.keys() for district in data[province] for sector in data[province][district]]
     except:
         return None
+
+def get_cells(province = '', district = '', sector = ''):
+    try:
+        if province and district and sector and get_sectors(province, district):
+            return [cell for cell in data[format_input(province)][format_input(district)][format_input(sector)].keys()]
+        if not province and not district and not sector:
+            return [cell for province in data.keys() for district in data[province] for sector in data[province][district] for cell in data[province][district][sector]]
+    except:
+        return None
+
+def get_villages(province = '', district = '', sector = '', cell = ''):
+    try:
+        if province and district and sector and cell and get_cells(province, district, sector):
+            return [village for village in data[format_input(province)][format_input(district)][format_input(sector)][format_input(cell)]]
+        if not province and not district and not sector and not cell:
+            return [village for province in data.keys() for district in data[province] for sector in data[province][district] for cell in data[province][district][sector] for village in data[province][district][sector][cell]]
+    except:
+        return None
